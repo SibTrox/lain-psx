87263b3d69b48c2a597d9d5d008f82a6949b8493
SibTrox <enzonicolascorrea@gmail.com>
2021-02-13 22:37:58 -0300

    Correccion de errores}

--- routes/game.js
+++ routes/game.js
@@ -9,21 +9,32 @@ router.get('/', function(req, res, next) {
   res.redirect('/')
 });
 
-router.get('/:level', function(req, res, next) {
-  let level = req.params.level;
-  let LainResponse = Lain[level];
-  res.render('level-interface', { LainResponse : LainResponse});
+router.get('/:site', function(req, res, next) {
+  res.redirect('/')
 });
 
-router.get('/:level/:id', function(req, res, next) {
+router.get('/:site/:level', function(req, res, next) {
+  let site = req.params.site;
+  let level = req.params.level;
+  if (site == "site-a" || site == "site-b"){
+    let LainResponse = Lain[site][level];
+    site == "site-a" ? numeroDeNiveles=22 : numeroDeNiveles=13
+    res.render('level-interface', { LainResponse : LainResponse, level : level, site : site, numeroDeNiveles : numeroDeNiveles})
+  }else{
+    res.redirect('/')
+  }
+});
+
+router.get('/:site/:level/:id', function(req, res, next) {
+    let site = req.params.site
     let level = req.params.level;
     let id = req.params.id;
-    let LainResponse = Lain[level][id];
+    let LainResponse = Lain[site][level][id];
     let videoExist = LainResponse["Video"][0];
     if(videoExist){
-      res.render('archive-interface-video', { LainResponse : LainResponse});
+      res.render('archive-interface-video', { LainResponse : LainResponse, level : level, site : site});
     }else{
-      res.render('archive-interface', { LainResponse : LainResponse});
+      res.render('archive-interface', { LainResponse : LainResponse, level : level, site : site});
     }
 });
   
